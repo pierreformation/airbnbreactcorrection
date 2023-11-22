1fe9c8bf48ddc0db108f3a78a3a2ee9f0f81df95
pierre <pierre@ND.fr>
2023-11-22 10:13:29 +0100

    put airbnb html and css and import images

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import './App.css';
-import imageSuperman from "./assets/Superman_shield.svg.png"
+import img1 from './assets/img1.webp'
+import img2 from './assets/img2.webp'
+import searchIcon from './assets/searchicon.png'
+import logo from './assets/logo.png'
 
 const App= () => {
 
@@ -7,9 +10,32 @@ const App= () => {
   console.log(toto)
 
   return (
-    <div className="myclass">
-      Test Hello
-      <img className='imageSuperman' src={imageSuperman}/>
+    <div className="App">
+     <header>
+        <img class="imgLogo" src={logo}/>
+        <div  class="searchBar"> 
+            <input/>
+            <img src={searchIcon}/>
+        </div>
+        <div class="headerWriting">
+            Mettre mon logement sur AirBnB
+        </div>
+      </header>
+      <section class="cardsContainer">
+          <div class="card">
+              <i class="fa-light fa fa-heart"></i>
+              <img src={img1}/>
+              <div>Texte</div>
+          </div>
+          <div class="card">
+              <img src={img2}/>
+              <div>Texte</div>
+          </div>
+          <div class="card">
+              <img src={img1}/>
+              <div>Texte</div>
+          </div>
+      </section>
     </div>
   );
 }
